c60751da0d256c108b778eb3d638526d32b5e1eb
mateus-abrantes <mateus.abrantes7@gmail.com>
2019-11-26 13:07:39 +0000

    Add server.py

--- ModbusClient.py
+++ ModbusClient.py
@@ -1,6 +1,31 @@
 from pyModbusTCP.client import ModbusClient
-try:
-    c = ModbusClient(host="localhost", port=502)
-    print("Deu certo")
-except ValueError:
-    print("Error with host or port params")
+import time
+
+SERVER_HOST = "localhost"
+SERVER_PORT = 502
+
+c = ModbusClient()
+
+# uncomment this line to see debug message
+#c.debug(True)
+
+# define modbus server host, port
+c.host(SERVER_HOST)
+c.port(SERVER_PORT)
+
+while True:
+    # open or reconnect TCP to server
+    if not c.is_open():
+        if not c.open():
+            print("unable to connect to "+SERVER_HOST+":"+str(SERVER_PORT))
+
+    # if open() is ok, read register (modbus function 0x03)
+    if c.is_open():
+        # read 10 registers at address 0, store result in regs list
+        regs = c.read_holding_registers(0, 10)
+        # if success display registers
+        if regs:
+            print("reg ad #0 to 9: "+str(regs))
+
+    # sleep 2s before next polling
+    time.sleep(2)
